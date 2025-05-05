21908f40492853ba0427eea236c949553ca8841a
harbargerdev <aharbarger@gmail.com>
2025-05-05 12:34:51 -0400

    Initial commit with devcontainer and a validation of the container

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from .validate_container import main
+
+# Call the main method from validate_container.py
+main()
---
+++ validate_container.py
@@ -0,0 +1,5 @@
+def main():
+    print("Dev container validation test successful!")
+
+if __name__ == "__main__":
+    main()
